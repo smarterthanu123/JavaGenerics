63520af080c6e7e9b8e3d379edae8a4bca0901dc
Aditya Kapoor <adityakapoor3252@gmail.com>
2024-01-28 17:15:09 +0530

    Java generics chapter 1 code examples

---
+++ chapter1/src/BoxingUnboxing.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
+// Small values -128 to +127 are cached and thus same boxed object might get returned.
+public class BoxingUnboxing {
+    public static void main(String[] args) {
+        List<Integer> bigs = Arrays.asList(100,200,300);
+        System.out.println(sumInteger(bigs) == sumInteger(bigs));
+
+        List<Integer> smalls = Arrays.asList(1,2,3);
+        System.out.println(sumInteger(smalls) == sumInteger(smalls));
+    }
+
+    public static Integer sumInteger(List<Integer> ints) {
+        Integer s = 0;
+        for (Integer n: ints) {
+            // Every n is first unboxed and s is unboxed everytime and then added and then boxed again and stored in s.
+            // A more optimized (and correct) implementation would be with int s and int n.
+            s+=n;
+        }
+        return s;
+    }
+}
---
+++ chapter1/src/ConvertAnyArrayToList.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertAnyArrayToList {
+    public static void main(String[] args) {
+        Integer[] arr = {1,2,3,4};
+        System.out.println(toList(arr));
+        List<Integer> ints = toList();
+        System.out.println(toList("Hello", "World"));
+        List<Comparable> comparables = ConvertAnyArrayToList.<Comparable>toList("Hello", 1);
+
+        // Use -ea in VM options in run configuration (Run > Edit Configurations) to enable assertions.
+        assert comparables.toString().equals("[Hello, 1]");
+    }
+
+    private static <T> List<T> toList(T... arr) {
+        List<T> list = new ArrayList<T>();
+        for(T ele: arr) {
+            list.add(ele);
+        }
+        return list;
+    }
+}
