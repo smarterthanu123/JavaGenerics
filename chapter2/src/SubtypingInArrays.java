cb953cd219bec5a2624dd512d8320a074ffb20e6
Aditya Kapoor <adityakapoor3252@gmail.com>
2024-02-08 22:34:31 +0530

    Adding subtyping in arrays

---
+++ chapter2/src/SubtypingInArrays.java
@@ -0,0 +1,12 @@
+public class SubtypingInArrays {
+    public static void main(String[] args) {
+        Integer[] arr = {1,2,3};
+        Number[] num;
+        num = arr; // Possible because array subtyping is covariant meaning type S[] is considered to subtype of T[]
+        // whenever S is subtype of T.
+
+        num[2] = 1.2; // Runtime error;
+
+        // There is no way to do something like <? super S> type of thing in Array.
+    }
+}
